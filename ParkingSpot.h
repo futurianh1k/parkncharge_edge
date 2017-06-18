4fe0e0bc0e7d2bede369494de26416b949ef91cc
Seongdo Kim <sdland85@gmail.com>
2017-06-18 13:18:20 -0700

    Copyright added

--- ParkingSpot.h
+++ ParkingSpot.h
@@ -1,3 +1,18 @@
+// File:	ParkingSpot.h
+// Author:	Seongdo Kim
+// Contact:	sdland85@gmail.com
+//
+// Copyright (c) 2017, Seongdo Kim <sdland85@gmail.com>
+// All rights reserved.
+// The copyright to the computer program(s) herein is
+// the property of Seongdo Kim. The program(s) may be
+// used and/or copied only with the written permission
+// of Seongdo Kim or in accordance with the terms and
+// conditions stipulated in the agreement/contract under
+// which the program(s) have been supplied.
+//
+// Written by Seongdo Kim <sdland85@gmail.com>, June, 2017
+
 #pragma once
 
 #include <string>
