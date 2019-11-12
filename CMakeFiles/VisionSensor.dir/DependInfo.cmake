d3629cb1a90c274175b9dabd0efe752ea186e2e8
lhju4e <today1186@naver.com>
2019-11-12 13:45:07 +0900

    commit start Visionsensor with 3model

---
+++ CMakeFiles/VisionSensor.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/BoardController.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/BoardController.cpp.o"
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CameraInfo.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/CameraInfo.cpp.o"
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/IMessageData.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/IMessageData.cpp.o"
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/INetworkHandler.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/INetworkHandler.cpp.o"
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/IOUtils.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/IOUtils.cpp.o"
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/MainInterface.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/MainInterface.cpp.o"
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/MessageQueue.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/MessageQueue.cpp.o"
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/ParkingSpot.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/ParkingSpot.cpp.o"
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/ParkingSpotManager.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/ParkingSpotManager.cpp.o"
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/ParkingUpdateMessage.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/ParkingUpdateMessage.cpp.o"
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/SecureClient.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/SecureClient.cpp.o"
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/SensorInfo.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/SensorInfo.cpp.o"
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/SerialVideoReader.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/SerialVideoReader.cpp.o"
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/ServerNetworkHandler.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/ServerNetworkHandler.cpp.o"
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/ServerSyncMessage.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/ServerSyncMessage.cpp.o"
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/Settings.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/Settings.cpp.o"
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/TCPSocketListener.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/TCPSocketListener.cpp.o"
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/Utils.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/Utils.cpp.o"
+  "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/main.cpp" "/media/qisens/4tb1/JH/juhee/11-07/VisionSensor_3model/CMakeFiles/VisionSensor.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/glog"
+  "/usr/include/python3.6"
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/VisionSensor.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VisionSensor.dir/main.cpp.o"
+  "CMakeFiles/VisionSensor.dir/MainInterface.cpp.o"
+  "CMakeFiles/VisionSensor.dir/BoardController.cpp.o"
+  "CMakeFiles/VisionSensor.dir/CameraInfo.cpp.o"
+  "CMakeFiles/VisionSensor.dir/IMessageData.cpp.o"
+  "CMakeFiles/VisionSensor.dir/INetworkHandler.cpp.o"
+  "CMakeFiles/VisionSensor.dir/IOUtils.cpp.o"
+  "CMakeFiles/VisionSensor.dir/MessageQueue.cpp.o"
+  "CMakeFiles/VisionSensor.dir/ParkingSpot.cpp.o"
+  "CMakeFiles/VisionSensor.dir/ParkingSpotManager.cpp.o"
+  "CMakeFiles/VisionSensor.dir/ParkingUpdateMessage.cpp.o"
+  "CMakeFiles/VisionSensor.dir/SecureClient.cpp.o"
+  "CMakeFiles/VisionSensor.dir/SensorInfo.cpp.o"
+  "CMakeFiles/VisionSensor.dir/SerialVideoReader.cpp.o"
+  "CMakeFiles/VisionSensor.dir/ServerNetworkHandler.cpp.o"
+  "CMakeFiles/VisionSensor.dir/ServerSyncMessage.cpp.o"
+  "CMakeFiles/VisionSensor.dir/Settings.cpp.o"
+  "CMakeFiles/VisionSensor.dir/TCPSocketListener.cpp.o"
+  "CMakeFiles/VisionSensor.dir/Utils.cpp.o"
+  "VisionSensor.pdb"
+  "VisionSensor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VisionSensor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
